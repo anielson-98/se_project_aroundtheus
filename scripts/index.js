8b73d4a1512a4e14ef82f7ba90ea7fb2518a7bf5
Alex Nielson <alex.nielson@icloud.com>
2022-11-18 22:06:22 -0700

    Stage 4

--- scripts/index.js
+++ scripts/index.js
@@ -48,3 +48,73 @@ editButton.addEventListener("click", (evt) => {
 closeButton.addEventListener("click", (evt) => {
   closeModal();
 });
+
+// Find profile elements
+const profileName = document.querySelector(".profile__user");
+
+const profileDescription = document.querySelector(".profile__description");
+
+// Find form input elements
+const formInputName = document.querySelector("#name");
+
+const formInputDescription = document.querySelector("#description");
+
+// Fill the "Name" and "About me" fields with the values displayed on the page
+formInputName.value = profileName.textContent;
+
+formInputDescription.value = profileDescription.textContent;
+
+// Find the form in the DOM
+const profileFormElement = document.querySelector(".modal__form");
+
+// Form submission handler
+function handleProfileFormSubmit(e) {
+  // Prevent browser default behavior
+  e.preventDefault();
+
+  // Insert form values and display them on the page
+  profileName.textContent = formInputName.value;
+
+  profileDescription.textContent = formInputDescription.value;
+
+  // Close the modal
+  toggleModal();
+}
+
+// Connect the handler to the form and watch for the submit event
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+// Card element retriever
+function getCardElement(data) {
+  // Find the card template
+  let cardTemplate = document.querySelector("#card-element").content;
+
+  // Clone the content of the template tag
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  // Find the card title and image elements
+  let cardTitle = cardElement.querySelector(".card__title");
+  let cardImage = cardElement.querySelector(".card__image");
+
+  // Fill in the data's name and link to the corresponding fields
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+
+  // Return the ready HTML element with the filled-in data
+  return cardElement;
+}
+
+function createCardsList(card) {
+  // Find the cards list
+  const cardsList = document.querySelector(".cards__list");
+
+  // Create the new card element
+  const newCardElement = getCardElement(card);
+
+  // Prepend the new cards
+  cardsList.prepend(newCardElement);
+}
+
+// Create cards list
+initialCards.forEach(createCardsList);
